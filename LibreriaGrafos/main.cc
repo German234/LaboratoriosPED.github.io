4898528ab9adc477a95e01e661648ed66cea3569
JosseJuventino <00048322@uca.edu.sv>
2024-10-30 10:06:42 -0600

    feat: Add library graph

---
+++ LibreriaGrafos/main.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+#include "Grafos.hpp"
+
+struct Person {
+  std::string name;
+  int age;
+
+  // Sobrecargar el operador
+  bool operator<(const Person& other) const {
+    return name < other.name || (name == other.name && age < other.age);
+  }
+};
+
+int main() {
+  Graph<Person> graph;
+
+  Person alice{"Alice", 30};
+  Person bob{"Bob", 25};
+  Person charlie{"Charlie", 35};
+
+  graph.add(alice, bob);
+  graph.add(bob, charlie);
+
+  if (graph.find(bob)) {
+    std::cout << "Bob está en el grafo." << std::endl;
+  }
+
+  std::cout << "Recorrido DFS desde Alice:" << std::endl;
+  graph.DFS(alice);
+
+  return 0;
+}
